eabe3cd1069a48580ae86809ce3599697f2f792b
Stefan Maierhofer <sm@stefanmaierhofer.com>
2020-04-29 17:31:12 +0200

    update build system

--- build.fsx
+++ build.fsx
@@ -1,19 +1,23 @@
+#r "paket: groupref Build //"
+#load ".fake/build.fsx/intellisense.fsx"
 #load @"paket-files/build/aardvark-platform/aardvark.fake/DefaultSetup.fsx"
 
-open Fake
 open System
 open System.IO
 open System.Diagnostics
 open Aardvark.Fake
-open Fake.Testing
 
-do MSBuildDefaults <- { MSBuildDefaults with Verbosity = Some Minimal }
 do Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
 DefaultSetup.install ["src/Uncodium.SimpleStore.sln"]
+
 
 #if DEBUG
 do System.Diagnostics.Debugger.Launch() |> ignore
 #endif
 
+
 entry()
+
+
+
