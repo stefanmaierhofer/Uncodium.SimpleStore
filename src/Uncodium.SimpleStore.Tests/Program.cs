87f0086ad93a439cb9536841cb95e28a55417743
Stefan Maierhofer <sm@stefanmaierhofer.com>
2020-04-29 17:48:25 +0200

    update NUnit

---
+++ src/Uncodium.SimpleStore.Tests/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uncodium.SimpleStore.Tests
+{
+    class Program
+    {
+        static void Main()
+        {
+            var dbDiskLocation = @"/some/path/to/nowhere";
+            using (var store = new SimpleDiskStore(dbDiskLocation, null))
+            {
+                Console.WriteLine(store.Get("23531522-e181-4e4f-985f-4927296df9b1")?.Length ?? -1);
+            }
+        }
+    }
+}
