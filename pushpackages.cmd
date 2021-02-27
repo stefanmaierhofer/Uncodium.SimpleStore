90b2a04be29482e08e514837f64a9ff937b8aa88
Stefan Maierhofer <sm@stefanmaierhofer.com>
2021-02-27 16:48:23 +0100

    fix pushpackages.cmd

--- pushpackages.cmd
+++ pushpackages.cmd
@@ -8,4 +8,6 @@ git tag %1
 git push --tags
 
 dotnet paket pack bin --version %1
-dotnet nuget push "bin\*.%1.nupkg" --skip-duplicate --source https://www.nuget.org/api/v2/package
+
+set /p Key=<%HOMEPATH%\.ssh\nuget_uncodium.key
+dotnet nuget push "bin\*.%1.nupkg" --skip-duplicate --source https://www.nuget.org/api/v2/package -k %Key%
